7eac22ba2abbfdfe2522f05594473a9a3d5fbde3
Andrey Smirnov <Andrey.Smirnov@ingrammicro.com>
2023-07-19 23:08:51 +0300

    simplified suspendable code cleanup

--- hello/src/main/kotlin/learn/atomic/coroutines/SimpleSuspendableMain.kt
+++ hello/src/main/kotlin/learn/atomic/coroutines/SimpleSuspendableMain.kt
@@ -2,13 +2,8 @@ package learn.atomic.coroutines
 
 import kotlin.coroutines.suspendCoroutine
 
-data class Product(val name: String, val price: Long)
-
 suspend fun main() {
     log("started")
-
-    val product = Product("bread", 100)
-    println(product)
 
     suspendCoroutine<Unit> { continuation ->
         Coroutines.resumeCoroutineAfterDelay(continuation, 1000)
