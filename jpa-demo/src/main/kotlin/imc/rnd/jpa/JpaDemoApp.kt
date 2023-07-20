6d2a6f8c1b62266c8b28e22b712786588ea5a1d5
Andrey Smirnov <Andrey.Smirnov@ingrammicro.com>
2023-07-20 10:31:21 +0300

    query timeout experiments

--- jpa-demo/src/main/kotlin/imc/rnd/jpa/JpaDemoApp.kt
+++ jpa-demo/src/main/kotlin/imc/rnd/jpa/JpaDemoApp.kt
@@ -2,6 +2,7 @@ package imc.rnd.jpa
 
 import imc.rnd.jpa.model.DelayedJob
 import imc.rnd.jpa.sys.Jpa
+import org.hibernate.jpa.QueryHints
 
 fun main() {
   Jpa("jdbc:postgresql://localhost:5432/jpademo").use { jpa ->
@@ -10,7 +11,7 @@ fun main() {
       val newDelayedJob = DelayedJob(1)
       em.persist(newDelayedJob)
 
-      for (taskId in 2..10_000) {
+      for (taskId in 2..10) {
         em.persist(DelayedJob(taskId))
 
         if (taskId % 100 == 0) {
@@ -20,9 +21,20 @@ fun main() {
         }
       }
 
-      // reading delayed job
-      val delayedJob = em.find(DelayedJob::class.java, 1)
+      // reading delayed job with 1 second timeout
+      val delayedJob = em.find(DelayedJob::class.java, 1, mapOf(QueryHints.SPEC_HINT_TIMEOUT to 1000))
       println(delayedJob)
+
+      // slow query emulation, pg_sleep is called for every row
+      val query = em.createQuery(
+        "select dj from DelayedJob dj where function('pg_sleep', 1) is not null",
+        DelayedJob::class.java)
+
+      val delayedJobs = query
+        .setHint(QueryHints.SPEC_HINT_TIMEOUT, 1000)
+        .resultList
+
+      println(delayedJobs)
     }
   }
 }
--- jpa-demo/src/main/kotlin/imc/rnd/jpa/model/DelayedJob.kt
+++ jpa-demo/src/main/kotlin/imc/rnd/jpa/model/DelayedJob.kt
@@ -1,11 +1,25 @@
 package imc.rnd.jpa.model
 
+import org.hibernate.annotations.Loader
 import java.sql.Timestamp
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
+@Table(name = "delayed_job")
+@NamedNativeQuery(
+    name = "DelayedJob.byTaskId",
+    query = "select task_id, start_on, node_name, pg_sleep(3) from delayed_job where task_id = ?",
+    resultSetMapping = "DelayedJob.allColumns")
+@SqlResultSetMapping(
+    name = "DelayedJob.allColumns",
+    entities = [
+        EntityResult(
+            entityClass = DelayedJob::class,
+            fields = arrayOf(
+                FieldResult(name = "taskId", column = "task_id"),
+                FieldResult(name = "startOn", column = "start_on"),
+                FieldResult(name = "nodeName", column = "node_name")))])
+@Loader(namedQuery = "DelayedJob.byTaskId")
 class DelayedJob() {
     @Id
     @Column(name = "task_id")
